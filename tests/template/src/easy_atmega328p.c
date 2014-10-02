32dab02278cdb4026d587881726a9140e0670857
Christian D. Valenti <christian.valenti@temple.edu>
2014-10-02 18:24:51 -0400

    intial commit to start repo

---
+++ tests/template/src/easy_atmega328p.c
@@ -0,0 +1,66 @@
+#include <easy_atmega328p.h>
+
+void toggle(char port, int bit)
+{
+    if(port == 'A'){
+      //PORTA ^= (1<<bit);
+    }else if (port == 'B'){
+      PORTB ^= (1<<bit);
+    }else if (port == 'C'){
+      PORTC ^= (1<<bit);
+    }else if (port == 'D'){
+      PORTD ^= (1<<bit);
+    }
+}
+
+void bit_on(char port, int bit)
+{
+    if(port == 'A'){
+      //PORTA |= (1<<bit);
+    }else if (port == 'B'){
+      PORTB |= (1<<bit);
+    }else if (port == 'C'){
+      PORTC |= (1<<bit);
+    }else if (port == 'D'){
+      PORTD |= (1<<bit);
+    }
+}
+
+void bit_off(char port, int bit)
+{
+    if(port == 'A'){
+      //PORTA &= ~(1<<bit);
+    }else if (port == 'B'){
+      PORTB &= ~(1<<bit);
+    }else if (port == 'C'){
+      PORTC &= ~(1<<bit);
+    }else if (port == 'D'){
+      PORTD &= ~(1<<bit);
+    }
+}
+
+void set_as_input(char port, int bit)
+{
+    if(port == 'A'){
+      //DDRA &= ~(1<<bit);
+    }else if (port == 'B'){
+      DDRB &= ~(1<<bit);
+    }else if (port == 'C'){
+      DDRC &= ~(1<<bit);
+    }else if (port == 'D'){
+      DDRD &= ~(1<<bit);
+    }
+}
+
+void set_as_output(char port, int bit)
+{
+    if(port == 'A'){
+      //DDRA |= (1<<bit);
+    }else if (port == 'B'){
+      DDRB |= (1<<bit);
+    }else if (port == 'C'){
+      DDRC |= (1<<bit);
+    }else if (port == 'D'){
+      DDRD |= (1<<bit);
+    }
+}
